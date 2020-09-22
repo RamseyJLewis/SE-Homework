db018d09258038382459d12638c1b129e4c689d1
Ramsey lewis <ramseylewis99@gmail.com>
2020-09-22 09:12:02 -0400

    homework 9.18.20

--- HomeWorkWeek-1.js
+++ HomeWorkWeek-1.js
@@ -286,3 +286,8 @@ var sum = nums. reduce(function(a, b){
     }, 0);
     
     console. log(sum); 
+
+//     Definition and Usage
+// The reduce() method reduces the array to a single value.
+// The reduce() method executes a provided function for each value of the array (from left-to-right).
+// The return value of the function is stored in an accumulator (result/total).
