6fa04973a6c0932883fb6a4d44c918abaa48b39e
Ramsey lewis <ramseylewis99@gmail.com>
2020-09-22 23:43:30 -0400

    H.W-Loops-9.22.20

--- H.W.W-2.js
+++ H.W.W-2.js
@@ -7,14 +7,15 @@
 //          - Hi!
 //          - Hi!
 var str = 'Hi!'
-function greeting(){
 
+function greeting(){
     for( var i = 0; i < 5; i++){
-        //console.log('Hi!')
-     }
-    return 'loop'
+        console.log('Hi!')
     }
-   // console.log(greeting())
+    return 'loop'
+}
+
+console.log(greeting())
     
 
 
@@ -26,13 +27,12 @@ function greeting(){
 //          - Loop Count: 3
 //          - Etc.. based on the max value parameter\
 function loop (){
-for (var i = 1; i < 4; i++){
-
-    // console.log(`Loop Count ${i}`)
-} 
-
+    for (var i = 1; i < 4; i++){
+        console.log(`Loop Count ${i}`)
+    } 
 }
-// console.log(loop())
+
+console.log(loop())
 
 // Q3: Loop over the following array and console.log the current index value
 //      - ['Zeus', 'Hades', 'Hermies', 'Poseidon']
@@ -42,13 +42,15 @@ for (var i = 1; i < 4; i++){
 //          -   Hermies
 //          -   Poseidon
 var gods = ['Zeus', 'Hades', 'Hermies', 'Poseidon']
-    function godsList(){
-        for (var i = 0; i < gods.length; i++){
-        //console.log(gods[i])
-        }
-        return 'allGodsLogged'
+
+function godsList(){
+    for (var i = 0; i < gods.length; i++){
+        console.log(godsList[i])
     }
-   // console.log(godsList())
+    return 'allGodsLogged'
+}
+
+console.log(godsList())
 
 // Q4: Loop over an array of strings and return a new array of single letter strings
     // Input:    ['As', 'Soon', 'As' 'Possible']
@@ -60,46 +62,32 @@ var gods = ['Zeus', 'Hades', 'Hermies', 'Poseidon']
         // get first letter of each element of function
         //log
 
-    var  sentInputArray = ['As', 'Soon', 'As', 'Possible']
-      
-    function sentInput(){
-           var outPut = [  ]
-            
-           for (var i = 0; i < sentInputArray.length; i++){
-                var Input = sentInputArray[i]
-                 var midTree = Input.charAt(0)
-
-                 outPut.push(midTree)
-
-               
-                 //console.log(outPut)
-
-                
-
-                
-  
-
-            }
-            return outPut
-        }
-        
-        // console.log(sentInput())
+ var  sentInputArray = ['As', 'Soon', 'As', 'Possible' ]   
+function sentInput(){
+    var outPut = [  ]
+    for (var i = 0; i < sentInputArray.length; i++){
+        var Input = sentInputArray[i]
+        var midTree = Input.charAt(0)
+        outPut.push(midTree)
+    }
+    return outPut
+}      
+console.log(sentInput())
 
 // Q5: Loop over an array and log to the CLI an array where each word has its second letter capitalized
     // - Input:    ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
     // - Output: ['APple', 'BAnana', 'ORange', 'PEach', 'STrawberry', 'PLum'];
-   var fruits = ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
-    function fruitCap(){
-        for (var i = 0; i < fruits.length; i++){
-            var currentFruits = fruits[i];
-            var upperCase = fruits[i].charAt(1).toUpperCase();
-            var endOfSting = currentFruits.slice(2);
-            var newString = fruits[i][0] + upperCase + endOfSting;
-            fruits[i] = newString;
-            
-        }
+ var fruits = ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
+function fruitCap(){
+    for (var i = 0; i < fruits.length; i++){
+        var currentFruits = fruits[i];
+        var upperCase = fruits[i].charAt(1).toUpperCase();
+        var endOfSting = currentFruits.slice(2);
+        var newString = fruits[i][0] + upperCase + endOfSting;
+        fruits[i] = newString;
     }
-    fruitCap()
+}
+fruitCap()
     // console.log(fruits)
     
     // Q6: Loop over an array of names and log the first and last initial of each person
@@ -107,7 +95,9 @@ var gods = ['Zeus', 'Hades', 'Hermies', 'Poseidon']
     // - Output: ['SE', 'RL', 'SPL', 'WPL' ]
    //sperate first Initial
     //log the first
-var nameLst = ['Shamika Earle', 'Ramsey Lewis', 'Samuel Pierre Louis', 'Wilson Pierre Louis' ]
+
+
+    var nameLst = ['Shamika Earle', 'Ramsey Lewis', 'Samuel Pierre Louis', 'Wilson Pierre Louis' ]
 function lstInitial(arr){
     var outPut = [];
     for(var i = 0; i < arr.length; i++){      
