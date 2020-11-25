386be90e87e56fa4a007f92f5bbbad9f32d2f598
Ramsey lewis <ramseylewis99@gmail.com>
2020-11-24 22:19:02 -0500

    notes

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,17 +1,63 @@
 import React from 'react';
 
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org/',
+    author: 'Jordan Walke',
+    nus_comments: 3,
+    point: 4,
+    objectID: 0,
+},
+{
+title:'Redux',
+url:'https://redux.js.org/',
+author:'Dan Abramov, Andrew Clark',
+num_comments: 2,
+points:5,
+objectID:1,
+}
+];
 const welcome = {
   greeting: 'Hey',
  title: 'React',
 }
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello World</h1>
-      <label htmlFor='search'>Search: </label>
-      <input id='Search' type='text'/>
-    </div>
-  );
-}
 
-export default App;
+const App = () =>  {
+  const stories = [ ... ];
+
+    const [search, setSearchTerm] = React.useState('');
+
+    const handleChange = event => { 
+      setSearchTerm(event.target.value);
+    };
+
+  return (
+    <div>
+      <h1> My Hacker Stories </h1>
+
+      <label htmlFor='Search'> Search: </label>
+      <input id='search' type='text' onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+      
+      <hr />
+
+      <List list={stories}/>
+    </div>
+  ); 
+}; 
+
+    const List = props => 
+      props.list.map(item =>(
+          <div key ={item.objectID}>
+            <span>
+              <a href= {item.url}>{item.item} </a>
+            </span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points} </span>
+      </div>
+     ));
